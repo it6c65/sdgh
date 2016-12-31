f9525162cdda82ca2a04c7dd496dee9204826485
Luis Ilarraza <ilarrazaluis82@gmail.com>
2016-12-31 17:25:25 -0400

    Borrando el php anterior

--- default/app/borrar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "config.php";
-if(isset($_POST["herramienta"])){
-   $id_h=$_POST["herramienta"];
-   $borrar="delete from ".$tabla_h." where id_herramientas=".$id_h;
-   $borrarsql=mysql_query($borrar,$conex) or die("ERROR!".mysql_error());
-}
-
-?>
--- default/app/login.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include "config.php";
-if (isset($_POST["usuario"])){
-    $user=$_POST["usuario"];
-    $pass=$_POST["clave"];
-    $checking="hola";
-}
-?>
